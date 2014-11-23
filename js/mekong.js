fc296ddb363904ddc099557e48d683f57754cba2
huongtt <huongtthy@gmail.com>
2014-11-23 18:11:43 +0700

    Update slider

--- js/mekong.js
+++ js/mekong.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
     $("select").uniform();
-     $( "#tabs" ).tabs();
+    $( "#tabs" ).tabs();
+    $( "#tabs-project" ).tabs();
     $( "#tabs-support" ).tabs();
+    $( "#tabs-partner" ).tabs();
     $('.bxslider').bxSlider();
 });
